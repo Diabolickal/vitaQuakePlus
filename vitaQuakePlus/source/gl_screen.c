458abf92b62d0f827d30bbbe36b5f5822461ba28
Nathan <the-guitar-dude@hotmail.com>
2018-07-26 14:38:41 -0500

    Minor Bugfixes

--- vitaQuakePlus/source/gl_screen.c
+++ vitaQuakePlus/source/gl_screen.c
@@ -623,6 +623,7 @@ void SCR_ScreenShot_f (void)
 	buffer[16] = 24;	// pixel size
 
 	//glReadPixels (glx, gly, glwidth, glheight, GL_RGB, GL_UNSIGNED_BYTE, buffer+18 ); 
+	glReadPixels (glx, gly, glwidth, glheight, GL_RGB, GL_UNSIGNED_BYTE, buffer+18 ); 
 
 	// swap rgb to bgr
 	c = 18+glwidth*glheight*3;
